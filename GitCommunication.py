94147c2886a7b096417a84d3dbea715e25e0dec1
LindulaG <66986963+Lindula1@users.noreply.github.com>
2024-07-22 22:07:32 +1000

    patch 11

--- GitCommunication.py
+++ GitCommunication.py
@@ -8,20 +8,22 @@ from cryptography.fernet import Fernet
 with open('keyFile.key', 'rb') as filekey:
     key = filekey.read()
     fernet = Fernet(key)
-bytekey = b'gAAAAABmnkj_NUTQ3HzZsvc-VFYRuXVhci_yvM-IBSsCx_isEh3jI8AhhcxKZms84KHfsjcKqU7r-NUghxV1YvVY_0z_EZg904LditoRTW31orrBdf4ok3QWWLeWvj4HU9ZOlY663Qjs'
+gistByteKey = b'gAAAAABmnkojGGYz12iuRtPvCKvPPCclP--e9_pV8fD9_D8ah-pQZo5tYmxnwMBjwGtymDSbveHtS-M8yZcaq2YiAM9obAB4M-qhqyGkSLgAid5G1c_04pB3yUF8nzmZjxsoft20i108'
+byteKey = b'gAAAAABmnkj_NUTQ3HzZsvc-VFYRuXVhci_yvM-IBSsCx_isEh3jI8AhhcxKZms84KHfsjcKqU7r-NUghxV1YvVY_0z_EZg904LditoRTW31orrBdf4ok3QWWLeWvj4HU9ZOlY663Qjs'
 
-token = str(fernet.decrypt(bytekey).decode("utf-8"))
+token = str(fernet.decrypt(byteKey).decode("utf-8"))
 git = Github(token)
+gistKey = str(fernet.decrypt(gistByteKey).decode("utf-8"))
 
 def ErrorCheck():
     try:
-        gist = git.get_gist("f08ed4ef55b029514b988ef53c4506af")
+        gist = git.get_gist(gistKey)
         return False
     except github.GithubException:
         return True
 
 def UpdateGist(data):
-    gist = git.get_gist("f08ed4ef55b029514b988ef53c4506af")
+    gist = git.get_gist(gistKey)
     gist.edit("", {"Accounts.json": github.InputFileContent(data)})
 
 def LoadGist():
